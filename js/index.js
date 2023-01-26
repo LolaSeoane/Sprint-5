4ae86855788897c629569974b9abc5e2c3dccd7b
lola Seoane <lola.seoane@advnz.de>
2023-01-26 11:41:02 +0100

    Exercici-1

--- js/index.js
+++ js/index.js
@@ -2,9 +2,10 @@
 const API_URL = 'https://icanhazdadjoke.com';
 const HTMLResponse = document.querySelector('result');
 const header = {headers: {Accept: "application/json" }};
+function nextYoke(){
 fetch(API_URL, header).then((response) => response.json()).then((data) => {
     const joke = data.joke;
-    //document.getElementById('result')!.innerHTML = joke
     console.log(joke);
     return joke
 });
+}
