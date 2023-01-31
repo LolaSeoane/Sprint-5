5fbb2692818a531fd2f901fa155a42f32ccd12a7
lola Seoane <lola.seoane@advnz.de>
2023-01-31 16:31:33 +0100

    Exercici3 completat

--- js/index.js
+++ js/index.js
@@ -1,12 +1,30 @@
 "use strict";
-//const HTMLResponse = document.querySelector('result')
+const reportJokes = [];
 function nextJoke() {
-    const header = { headers: { Accept: "application/json" } };
     const API_URL = 'https://icanhazdadjoke.com';
+    const header = { headers: { Accept: "application/json" } };
+    const scoreButton = document.getElementById("scoreButtons");
+    scoreButton.style.display = "";
     fetch(API_URL, header).then((response) => response.json()).then((data) => {
         const joke = data.joke;
         console.log(joke);
         document.getElementById('result').innerHTML = joke;
         return joke;
     });
+}
+function getPoints(id) {
+    var _a;
+    const punctuation = id;
+    const textDate = new Date().toISOString();
+    const lastJoke = (_a = document.getElementById('result')) === null || _a === void 0 ? void 0 : _a.outerText;
+    const textLastJoke = lastJoke;
+    const jokeIndex = reportJokes.findIndex((e) => textLastJoke === e.joke);
+    const jokes = { joke: textLastJoke, score: punctuation, date: textDate };
+    if (textLastJoke !== '' && jokeIndex < 0) {
+        reportJokes.push(jokes);
+    }
+    else {
+        reportJokes[jokeIndex].score = punctuation;
+    }
+    console.log(reportJokes);
 }
--- js/index.ts
+++ js/index.ts
@@ -1,8 +1,16 @@
 
-//const HTMLResponse = document.querySelector('result')
+const reportJokes: any = []
+
 function nextJoke() {
-    const header = { headers: { Accept: "application/json" } }
     const API_URL = 'https://icanhazdadjoke.com'
+    const header = { headers: { Accept: "application/json" } }
+
+    const scoreButton = document.getElementById(
+        "scoreButtons"
+    ) as HTMLButtonElement;
+
+    scoreButton.style.display = "";
+
 
     fetch(API_URL, header).then((response) => response.json()).then((data) => {
         const joke = data.joke
@@ -10,5 +18,23 @@ function nextJoke() {
         document.getElementById('result')!.innerHTML = joke
         return joke
     })
-
 }
+function getPoints(id: number) {
+    const punctuation: number = id
+    const textDate = new Date().toISOString()
+    const lastJoke: any = document.getElementById('result')?.outerText;
+    const textLastJoke: String = lastJoke
+    const jokeIndex = reportJokes.findIndex((e: any) => textLastJoke === e.joke);
+    const jokes = { joke: textLastJoke, score: punctuation, date: textDate }
+    
+    if (textLastJoke !== '' && jokeIndex < 0) {
+        reportJokes.push(jokes)
+    }
+    else {
+        reportJokes[jokeIndex].score = punctuation
+        
+    }
+    console.log(reportJokes)
+}
+
+
